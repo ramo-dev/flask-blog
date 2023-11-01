b269bac9b5988ebc16d89e2ebc8d9700da45980c
ramo-dev <annuar.dev@gmail.com>
2023-11-01 23:41:42 +0300

    run.py

--- run.py
+++ run.py
@@ -5,8 +5,5 @@ if __name__ == '__main__':
     app.app_context().push()
 
     # Run the Flask application
-<<<<<<< HEAD
+
     app.run(debug=False,host='0.0.0.0', port=80)
-=======
-    app.run(debug=False, host='0.0.0.0')
->>>>>>> refs/remotes/origin/main
