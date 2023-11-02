a21a726d7d2cc82e18a4c7eb5dd935381b42a5c5
ramo-dev <136629650+ramo-dev@users.noreply.github.com>
2023-11-02 19:48:20 +0300

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -19,8 +19,8 @@ app.config['MAIL_SERVER'] = "smtp.gmail.com"
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = 'noreplyfbtest@gmail.com'
+app.config['MAIL_PASSWORD'] = 'lkosbolnuhnjfgxc'
 
 mail = Mail(app)
 
